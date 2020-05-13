ea128a52477baa294d00926e30500db16b56704d
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-13 20:32:03 +0900

    KbnButtonのテスト

--- src/components/templates/KbnLoginView.vue
+++ src/components/templates/KbnLoginView.vue
@@ -1,1 +1,1 @@
-<template>\n <p>ログインページ</p>\n</template>
+<template>\n  <p>ログインページ</p>\n</template><template>\n  <p>ボードページ</p>\n</template>
--- test/unit/specs/components/atoms/KbnButton.spec.js
+++ test/unit/specs/components/atoms/KbnButton.spec.js
@@ -1,62 +1,82 @@
 import { mount } from '@vue/test-utils'
 import KbnButton from '@/components/atoms/KbnButton.vue'
 
-describe('KbnButton',() => {
-  describe('プロパティ',() => {
-    describe('デフォルト',() => {
-      it('kbn-buttonクラスを持つbutton要素で構成されること',() => {
-        const button = mount(KbnButton)
-        expect(button.is('button')).to.equal(true)
-        expect(button,classes()).to.include('kbn0button')
+describe('KbnButton', () => {
+  describe('プロパティ', () => {
+    describe('type', () => {
+      describe('デフォルト', () => {
+        it('kbn-buttonクラスを持つbutton要素で構成されること', () => {
+          const button = mount(KbnButton)
+          expect(button.is('button')).to.equal(true)
+          expect(button.classes()).to.include('kbn-button')
+        })
+      })
+
+      describe('button', () => {
+        it('kbn-buttonクラスを持つbutton要素で構成されること', () => {
+          const button = mount(KbnButton, {
+            propsData: { type: 'button' }
+          })
+          expect(button.is('button')).to.equal(true)
+          expect(button.classes()).to.include('kbn-button')
+        })
+      })
+
+      describe('text', () => {
+        it('kbn-button-textクラスを持つbutton要素で構成されること', () => {
+          const button = mount(KbnButton, {
+            propsData: { type: 'text' }
+          })
+          expect(button.is('button')).to.equal(true)
+          expect(button.classes()).to.include('kbn-button-text')
+        })
       })
     })
 
-    describe('button',() => {
-      it('kbn-buttonクラスを持つbutton要素で構成されること',() => {
+    describe('disabled', () => {
+      describe('デフォルト', () => {
+        it('disabled属性が付与されていないこと', () => {
+          const button = mount(KbnButton, {
+            propsData: { disabled: true }
+          })
+          expect(button.attributes().disabled).to.equal('disabled')
+        })
+      })
+
+      describe('false', () => {
+        it('disabled属性が付与されていないこと', () => {
+          const button = mount(KbnButton)
+          expect(button.attributes().disabled).to.be.an('undefined')
+        })
+      })
+    })
+  })
+
+  describe('イベント', () => {
+    describe('click', () => {
+      it('発行されていること', () => {
+        const button = mount(KbnButton)
+        button.trigger('click')
+        expect(button.emitted().click.length)
+      })
+    })
+  })
+
+  describe('スロット', () => {
+    describe('コンテンツ挿入あり', () => {
+      it('挿入すること', () => {
         const button = mount(KbnButton, {
-          propsData: { type: 'button'}
+          slots: { default: '<p>hello</p>' }
         })
-        expect(button.is('button')).to.equal(true)
-        expect(button.classes()).to.include('kbn-button')
+        expect(button.text()).to.equal('hello')
       })
     })
 
-    describe('text',() => {
-      it('kbn-button-textクラスを持つbutton要素で構成されること',() => {
-        const button = mount(KbnButton,{
-          propsData: {type: 'text'}
-        })
-        expect(button.is('button')).to.equal(true)
-        expect(button.classes()).to.include('kbn-button-text')
-      })
-    })
-  })
-
-  describe('disabled',() => {
-    describe('デフォルト', () => {
-      it('disabled属性が付与されていないこと', () => {
-        const button = mount(KbnButton,{
-          propsData: {disabled: true }
-        })
-        expect(button.attributes().disabled).to.equal('disabled')
-      })
-    })
-
-    describe('false',() => {
-      it('disabled属性が付与されていないこと', () => {
+    describe('コンテンツ挿入なし', () => {
+      it('挿入されていないこと', () => {
         const button = mount(KbnButton)
-        expect(button.attributes().disabled).to.be.an('undefined')
+        expect(button.text()).to.equal('')
       })
-    })
-  })
-})
-
-describe('イベント', () => {
-  describe('click', () => {
-    it('発行されていること', () => {
-      const button = mount(KbnButton)
-      button.trigger('click')
-      expect(button.emitted().click.length)
     })
   })
 })
