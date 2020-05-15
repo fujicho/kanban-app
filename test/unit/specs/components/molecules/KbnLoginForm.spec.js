83ea15af9c6f100c7ea3877e0cc0ec141a78c6ee
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-15 16:35:47 +0900

    軽微な修正

--- test/unit/specs/components/molecules/KbnLoginForm.spec.js
+++ test/unit/specs/components/molecules/KbnLoginForm.spec.js
@@ -4,7 +4,7 @@ import KbnLoginForm from '@components/molecules/KbnLoginForm.vue'
 describe('KbnLoginForm', () => {
   describe('プロパティ', () => {
     describe('validation', () => {
-      let KbnLoginForm
+      let loginForm
       beforeEach(done => {
         loginForm = mount(KbnLoginForm, {
           propsData: { onlogin: () => {} }
@@ -16,7 +16,7 @@ describe('KbnLoginForm', () => {
         describe('required', () => {
           describe('何も入力されていない', () => {
             it('validation.email.requiredがinvalidであること', () => {
-              loginForm.setData({ email: ''})
+              loginForm.setData({ email: '' })
               expect(loginForm.vm.validation.email.required).to.equal(false)
             })
           })
@@ -24,21 +24,21 @@ describe('KbnLoginForm', () => {
           describe('入力あり', () => {
             it('validation.email.requiredがvalidであること', () => {
               loginForm.setData({ email: 'foo@domain.com' })
-              expect(loginForm.vm.validation.emaik.required).to.equal(true)
+              expect(loginForm.vm.validation.email.required).to.equal(true)
             })
           })
         })
 
         describe('format', () => {
           describe('メールアドレス形式ではないフォーマット', () => {
-            it('validation.email.formatがinvalidであること',() => {
+            it('validation.email.formatがinvalidであること', () => {
               loginForm.setData({ email: 'foobar' })
               expect(loginForm.vm.validation.email.format).to.equal(false)
             })
           })
 
           describe('メールアドレス形式のフォーマット', () => {
-            it('validation.email.formatがvalidであること', () =>{
+            it('validation.email.formatがvalidであること', () => {
               loginForm.setData({ email: 'foo@domain.com' })
               expect(loginForm.vm.validation.email.format).to.equal(true)
             })
@@ -64,7 +64,7 @@ describe('KbnLoginForm', () => {
         })
       })
     })
-    
+
     describe('valid', () => {
       let loginForm
       beforeEach(done => {
@@ -74,98 +74,129 @@ describe('KbnLoginForm', () => {
         loginForm.vm.$nextTick(done)
       })
 
-      it('validになること', () => {
+      describe('バリデーション項目全てOK', () => {
+        it('validになること', () => {
+          loginForm.setData({
+            email: 'foo@domain.com',
+            password: '12345678'
+          })
+          expect(loginForm.vm.valid).to.equal(true)
+        })
+      })
+
+      describe('バリデーションNG項目あり', () => {
+        it('invalidになること', () => {
+          loginForm.setData({
+            email: 'foo@domain.com',
+            password: ''
+          })
+          expect(loginForm.vm.valid).to.equal(false)
+        })
+      })
+    })
+
+    describe('disableLoginAction', () => {
+      let loginForm
+      beforeEach(done => {
+        loginForm = mount(KbnLoginForm, {
+          propsData: { onlogin: () => {} }
+        })
+        loginForm.vm.$nextTick(done)
+      })
+
+      describe('バリデーションNG項目ある', () => {
+        it('ログイン処理は無効', () => {
+          loginForm.setData({
+            email: 'foo@domain.com',
+            password: ''
+          })
+          expect(loginForm.vm.disableLoginAction).to.equal(true)
+        })
+      })
+
+      describe('バリデーション項目すべてOKかつログイン処理中ではない', () => {
+        it('ログイン処理は有効', () => {
+          loginForm.setData({
+            email: 'foo@domain.com',
+            password: '12345678'
+          })
+          expect(loginForm.vm.disableLoginAction).to.equal(false)
+        })
+      })
+
+      describe('バリデーション項目すべてOKかつログイン処理中', () => {
+        it('ログイン処理は無効', () => {
+          loginForm.setData({
+            email: 'foo@domain.com',
+            password: '12345678',
+            progress: true
+          })
+          expect(loginForm.vm.disableLoginAction).to.equal(true)
+        })
+      })
+    })
+
+    describe('onlogin', () => {
+      let loginForm
+      let onloginStub
+      beforeEach(done => {
+        onloginStub = sinon.stub()
+        loginForm = mount(KbnLoginForm, {
+          propsData: { onlogin: onloginStub }
+        })
         loginForm.setData({
           email: 'foo@domain.com',
           password: '12345678'
         })
-        expect(loginForm.vm.valid).to.equal(true)
+        loginForm.vm.$nextTick(done)
       })
-    })
 
-    describe('バリデーションNG項目あり', () => {
-      it('invalidになること', () => {
-        loginForm.setData({
-          email: 'foo@domain.com',
-          password: ''
-        })
-        expect(loginForm.vm.valid).to.equal(false)
-      })
-    })
-  })
-
-  describe('disableLoginAction', () => {
-    let loginForm
-    beforeEach(done => {
-      loginForm = mount(KbnLoginForm, {
-        propsData: {onlogin: () => {} }
-      })
-      loginForm.vm.$nextTick(done)
-    })
-
-    describe('バリデーションNG項目ある', () => {
-      it('ログイン処理は無効', () => {
-        loginForm.setData({
-          email: 'foo@domain.com',
-          password: ''
-        })
-        expect(loginForm.vm.disableLoginAction).to.equal(true)
-      })
-    })
-
-    describe('バリデーション項目すべてOKかつログイン処理中ではない', () => {
-      it('ログイン処理は無効', () => {
-        loginForm.setData({
-          email: 'foo@domain.com',
-          password: '12345678',
-          progress: true
-        })
-        expect(loginForm.vm.disableLoginAction).to.equal(true)
-      })
-    })
-  })
-
-  describe('onlogin', () => {
-    let loginForm
-    let onloginStub
-    beforeEach(done => {
-      onloginStub = sinon.stub()
-      loginForm = mount(KbnLoginForm, {
-        propsData: { onlogin: onloginStub }
-      })
-      loginForm.setData({
-        email: 'foo@domain.com',
-        password: '12345678'
-      })
-      loginForm.vm.$nextTick(done)
-    })
-
-    describe('resolve', () => {
-      it('resolveされること', done => {
-        onloginStub.resolves()
-        
-        // クリックイベント
-        loginForm.find('button').trigger('click')
-        expect(onloginStub.called).to.equal(false)
-        expect(loginForm.vm.error).to.equal('')
-        expect(loginForm.vm.disableLoginAction).to.equal(true)
-
-        // 状態の反映
-        loginForm.vm.$nextTick(() => {
-          expect(onloginStub.called).to.equal(true)
-          const authInfo = onloginStub.args[0][0]
-          expect(authInfo.email).to.equal(loginForm.vm.email)
-          expect(authInfo.password).to.equal(loginForm.vm.password)
+      describe('resolve', () => {
+        it('resolveされること', done => {
+          onloginStub.resolves()
+          // クリックイベント
+          loginForm.find('button').trigger('click')
+          expect(onloginStub.called).to.equal(false)
+          expect(loginForm.vm.error).to.equal('')
+          expect(loginForm.vm.disableLoginAction).to.equal(true)
+          // 状態の反映
           loginForm.vm.$nextTick(() => {
-            expect(loginForm.vm.error).to.equal('')
-            expect(loginForm.vm.disableLoginAction).to.equal(false)
+            expect(onloginStub.called).to.equal(true)
+            const authInfo = onloginStub.args[0][0]
+            expect(authInfo.email).to.equal(loginForm.vm.email)
+            expect(authInfo.password).to.equal(loginForm.vm.password)
+            loginForm.vm.$nextTick(() => {
+              expect(loginForm.vm.error).to.equal('')
+              expect(loginForm.vm.disableLoginAction).to.equal(false)
 
-            done()
+              done()
+            })
+          })
+        })
+      })
+
+      describe('reject', () => {
+        it('rejectされること', done => {
+          onloginStub.rejects(new Error('login error!'))
+          // クリックイベント
+          loginForm.find('button').trigger('click')
+          expect(onloginStub.called).to.equal(false)
+          expect(loginForm.vm.error).to.equal('')
+          expect(loginForm.vm.disableLoginAction).to.equal(true)
+          // 状態の反映
+          loginForm.vm.$nextTick(() => {
+            expect(onloginStub.called).to.equal(true)
+            const authInfo = onloginStub.args[0][0]
+            expect(authInfo.email).to.equal(loginForm.vm.email)
+            expect(authInfo.password).to.equal(loginForm.vm.password)
+            loginForm.vm.$nextTick(() => {
+              expect(loginForm.vm.error).to.equal('login error!')
+              expect(loginForm.vm.disableLoginAction).to.equal(false)
+              done()
+            })
           })
         })
       })
     })
-
-    
   })
 })
