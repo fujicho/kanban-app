2343d0c77a755010cd4a3648843a634e57a2d987
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-17 11:35:11 +0900

    軽微な修正

--- src/components/molecules/KbnLoginForm.vue
+++ src/components/molecules/KbnLoginForm.vue
@@ -41,8 +41,8 @@
         ログイン中...
       </p>
       <p
-      v-if="error"
-      class="login-error"
+        v-if="error"
+        class="login-error"
       >
         {{ error }}
       </p>
@@ -52,7 +52,7 @@
 
 <script>
 // KbnButtonをインポート
-import KbnButton from "@/components/atoms/KbnButton"
+import KbnButton from '@/components/atoms/KbnButton'
 // メールアドレスのフォーマットをチェックする正規表現
 const REGEX_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const required = val => !!val.trim()
