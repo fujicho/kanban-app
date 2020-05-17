37b86ba813a888566124930ee2a5be7673a761ae
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-17 10:07:23 +0900

    templatesの修正

--- src/components/templates/KbnLoginView.vue
+++ src/components/templates/KbnLoginView.vue
@@ -1,15 +1,38 @@
 <template>
-  <p>ログインページ</p>
+  <div class="login-view">
+    <h1>kanban App</h1>
+    <KbnLoginForm :onlogin="handleLogin" />
+  </div>
 </template>
 
 
 
 <script>
-export default {
+import KbnLoginForm from '@/components/molecules/KbnLoginForm.vue'
 
+export default {
+  name: 'KbnLoginView',
+
+  components: {
+    KbnLoginForm
+  } ,
+
+  methods: {
+    handleLogin (authInfo) {
+      return this.$store.dispatch('login', authInfo)
+        .then(() => {
+          this.$router.push({ path: '/' })
+        })
+        .catch(err => this.throwReject(err))
+    },
+    throwReject (err) { return Promise.reject(err) }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.login-view {
+  width : 320px;
+  margin: auto;
+}
 </style>
