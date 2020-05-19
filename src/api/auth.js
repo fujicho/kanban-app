fb0795f8bb554fc2a68033e48ced0f806e378c45
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-19 21:32:13 +0900

    修正

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,7 +5,6 @@ export default {
     return new Promise((resolve, reject) => {
       client.post('/auth/login', authInfo)
         .then(res => resolve({ token: res.data.token, userId: res.data.userId }))
-
         .catch(err => {
           reject(new Error(err.response.data.message || err.message))
         })
